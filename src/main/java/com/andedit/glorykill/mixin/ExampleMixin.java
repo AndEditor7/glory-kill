059aae3ef7d1688a1df003e22402f46abe1781d0
AndEditor7 <andeditor7@gmail.com>
2021-09-27 10:52:55 +1000

    Change icon

--- src/main/java/com/andedit/glorykill/mixin/ExampleMixin.java
+++ src/main/java/com/andedit/glorykill/mixin/ExampleMixin.java
@@ -1,17 +1,18 @@
 package com.andedit.glorykill.mixin;
 
-import com.andedit.glorykill.ExampleMod;
-
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.andedit.glorykill.Main;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }

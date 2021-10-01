9c1e1d01ef30ad8051485e4695286552e478cbb8
AndEditor7 <andeditor7@gmail.com>
2021-10-01 21:15:33 +1000

    Add utility method

--- src/main/java/com/andedit/glorykill/extansion/WorldExt.java
+++ src/main/java/com/andedit/glorykill/extansion/WorldExt.java
@@ -4,7 +4,31 @@ import java.util.List;
 
 import com.andedit.glorykill.handle.GloryHandle;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
 public interface WorldExt {
 	List<GloryHandle> getGloryList();
+	
 	void addGlory(GloryHandle handle);
+	
+	default boolean containEntity(LivingEntity entity) {
+		for (var handle : getGloryList()) {
+			if (handle.entity == entity) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	default boolean containPlayer(PlayerEntity player) {
+		for (var handle : getGloryList()) {
+			if (handle.player == player) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }

026d36387d9cceab351d4e14cc6b12b2c552732a
AndEditor7 <andeditor7@gmail.com>
2021-10-01 20:17:54 +1000

    Add static networks class

---
+++ src/main/java/com/andedit/glorykill/networking/Networks.java
@@ -0,0 +1,9 @@
+package com.andedit.glorykill.networking;
+
+import com.andedit.glorykill.Main;
+
+import net.minecraft.util.Identifier;
+
+public class Networks {
+	public static final Identifier GLORYKILL = new Identifier(Main.ID, "glory_kill");
+}
